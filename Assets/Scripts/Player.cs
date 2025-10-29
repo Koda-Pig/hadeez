93b40f3e14f01fcc8bd36ec0c054a25bb9b5fc39
Josh Koter <josh.koter@gmail.com>
2025-10-30 00:24:39 +0200

    Collision detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,8 +14,69 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+
+        bool canMove = !Physics.CapsuleCast(
+            transform.position,
+            transform.position + Vector3.up * playerHeight,
+            playerRadius,
+            moveDirection,
+            moveDistance
+        );
+
+        if (!canMove)
+        {
+            // Cannot move towards this moveDirection
+            // Attempt only X movement
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+
+            canMove = !Physics.CapsuleCast(
+                transform.position,
+                transform.position + Vector3.up * playerHeight,
+                playerRadius,
+                moveDirectionX,
+                moveDistance
+            );
+
+            if (canMove)
+            {
+                // Can move on x-axis
+                moveDirection = moveDirectionX;
+            }
+            else
+            {
+                // cannot move only on the X axis, try Z
+                Vector3 moveDirectionZ = new Vector3(moveDirection.z, 0, 0).normalized;
+
+                canMove = !Physics.CapsuleCast(
+                    transform.position,
+                    transform.position + Vector3.up * playerHeight,
+                    playerRadius,
+                    moveDirectionZ,
+                    moveDistance
+                );
+
+                if (canMove)
+                {
+                    // can move only on the Z axis
+                    moveDirection = moveDirectionZ;
+                }
+                else
+                {
+                    // can't move in any direction
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDirection * moveDistance;
+        }
         isWalking = moveDirection != Vector3.zero;
+
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(
             transform.forward,
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Unity Game
 
-[CM Tut](https://youtu.be/AmGSEH7QcDg?si=258Soi2uLDatAPtj&t=7407)
+[CM Tut](https://youtu.be/AmGSEH7QcDg?si=258Soi2uLDatAPtj&t=8209)
 
 ## Notes
 
@@ -9,3 +9,18 @@
 3. Always remember to zero the position of new objects and things you add to the game (set position coordinates to 0, 0, 0)
 4. I've set the 'Active Input Handling' to 'Both', as per the recommendation of the tutorial, but against the recommendation of the Unity program itself. Unity recommends using the newer input manager.
 5. The tutorial instructs to create a new InputActions thing in the Project tab (named `PlayerInputActions`). I've not done so because there is an existing one that seems strongly tied to the new version of Unity. So I'm going to use the existing one (named `InputSystem_Actions`) instead. This is NB because the project often refers to `PlayerInputActions`, but in my case it will be `InputSystem_Actions`
+6. Jeez like object collisions are insanely simple in Unity. Basically just this code in the Player.cs class:
+
+   ```C#
+   float playerSize = .7f;
+   bool canMove = !Physics.Raycast(transform.position, moveDirection, playerSize);
+
+   if (canMove)
+   {
+   	transform.position += moveDirection * moveSpeed * Time.deltaTime;
+   }
+   ```
+
+   Coming from a game development background in vanilla JavaScript, where signifigant calculations are involved in detecting collisions, this is fantastic. See what I mean in [this game I made with just JS](https://github.com/Koda-Pig/playground/blob/main/knight-of-cups/script.js#L97). OK just progressed further in the tut and it was explained that the `Raycast` method detects collisions from the center of the player, not taking into account any player width or height. This is unsuitable for obvious reasons. Instead, a `CapsuleCast` is used, which is generally accepted as a good substitute for an actual player shape.
+
+7. NEXT!
