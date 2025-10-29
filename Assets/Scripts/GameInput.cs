4c9bd6a6708bd06fb4096e7ad31f96e28ddb0ebd
Josh Koter <josh.koter@gmail.com>
2025-10-29 23:35:39 +0200

    input refactor

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -3,27 +3,17 @@ using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
+    private InputSystem_Actions playerInputActions;
+
+    private void Awake()
+    {
+        playerInputActions = new InputSystem_Actions();
+        playerInputActions.Player.Enable();
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
-        Vector2 inputVector = new(0, 0);
-
-        if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
-        {
-            inputVector.y = +1;
-        }
-        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
-        {
-            inputVector.x = -1;
-        }
-        if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
-        {
-            inputVector.y = -1;
-        }
-        if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
-        {
-            inputVector.x = +1;
-        }
-
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
         inputVector = inputVector.normalized;
         return inputVector;
     }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Unity Game
 
-[CM Tut](https://youtu.be/AmGSEH7QcDg?si=258Soi2uLDatAPtj&t=6759)
+[CM Tut](https://youtu.be/AmGSEH7QcDg?si=258Soi2uLDatAPtj&t=7407)
 
 ## Notes
 
@@ -8,3 +8,4 @@
 2. You can only animate child objects. Another good reason to keep the visuals and logic separate.
 3. Always remember to zero the position of new objects and things you add to the game (set position coordinates to 0, 0, 0)
 4. I've set the 'Active Input Handling' to 'Both', as per the recommendation of the tutorial, but against the recommendation of the Unity program itself. Unity recommends using the newer input manager.
+5. The tutorial instructs to create a new InputActions thing in the Project tab (named `PlayerInputActions`). I've not done so because there is an existing one that seems strongly tied to the new version of Unity. So I'm going to use the existing one (named `InputSystem_Actions`) instead. This is NB because the project often refers to `PlayerInputActions`, but in my case it will be `InputSystem_Actions`
