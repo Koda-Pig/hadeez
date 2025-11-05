40768fe6453319202021d38dbfb620ec7df042a2
Josh Koter <josh.koter@gmail.com>
2025-11-05 21:44:01 +0200

    setup plate counter

---
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter
+{
+    private float spawnPlateTimer;
+    private float spawnPlateTimerMax = 4f;
+    private int platesSpawnedAmount;
+    private int platesSpawnedAmountMax = 4;
+
+    [SerializeField]
+    private KitchenObjectSO plateKitchenObjectSO;
+
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+    private void Update()
+    {
+        spawnPlateTimer += Time.deltaTime;
+        if (spawnPlateTimer > spawnPlateTimerMax)
+        {
+            // KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, this);
+            spawnPlateTimer = 0f;
+
+            if (platesSpawnedAmount < platesSpawnedAmountMax)
+            {
+                platesSpawnedAmount++;
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        // player already holding something
+        if (player.HasKitchenObject())
+        {
+            return;
+        }
+
+        // there's no plates available
+        if (platesSpawnedAmount == 0)
+        {
+            return;
+        }
+
+        platesSpawnedAmount--;
+        KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+        OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Unity Game
 
-[CM Tut](https://youtu.be/AmGSEH7QcDg?si=1jsnQWUKKulV43mJ&t=21365)
+[CM Tut](https://youtu.be/AmGSEH7QcDg?si=1jsnQWUKKulV43mJ&t=22090)
 
 ## Notes
 
